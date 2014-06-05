60bcf74fb7952063cd4f98db16344756649cc65a
Arik Devens <adevens@fitbit.com>
2014-06-04 18:14:33 -0700

    Initial import.

---
+++ Future/Future.h
@@ -0,0 +1,19 @@
+//
+//  Future.h
+//  Future
+//
+//  Created by Arik Devens on 6/4/14.
+//  Copyright (c) 2014 danieltiger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Future.
+FOUNDATION_EXPORT double FutureVersionNumber;
+
+//! Project version string for Future.
+FOUNDATION_EXPORT const unsigned char FutureVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Future/PublicHeader.h>
+
+
